7ff1142f699c53fd3a5dc2ff67bf4a551338f09f
M_Ridho <muhammadridho21058@gmail.com>
2025-05-10 19:50:44 +0700

    feat: route for admin pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RoleMiddleware;
 use App\Http\Controllers\Auth\AuthenticationController;
+// admin
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ItemController as AdminItemController;
+use App\Http\Controllers\Admin\UserController as AdminUserManagementController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +16,13 @@ Route::get('/', function () {
 Route::get('/login', [AuthenticationController::class, 'showFormLogin'])->name('login')->middleware('guest');
 Route::post('/login', [AuthenticationController::class, 'login']);
 Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout')->middleware('auth');
+
+// Admin
+Route::middleware(['auth', RoleMiddleware::class . ':admin'])
+    ->prefix('admin')->name('admin.')->group(function () {
+        Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+        // Route::get('/users', [AdminController::class, 'manageUsers'])->name('users.index');
+        Route::resource('users', AdminUserManagementController::class);
+        Route::resource('categories', CategoryController::class);
+        Route::resource('items', AdminItemController::class);
+});
